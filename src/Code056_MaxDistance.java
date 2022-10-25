cdf31a8b399a62cc187d00f211e150d173aad2a8
alex <alex@gmail.com>
2022-10-25 08:41:43 +0800

    最大节点距离 代码注释

--- src/Code056_MaxDistance.java
+++ src/Code056_MaxDistance.java
@@ -121,14 +121,14 @@ public class Code056_MaxDistance {
         }
     }
 
-    public static Info process(Node x){
-        if (x==null){
-            return new Info(0,0);
+    public static Info process(Node x) {
+        if (x == null) {
+            return new Info(0, 0);
         }
         Info leftInfo = process(x.left);
         Info rightInfo = process(x.right);
 
-        int height = Math.max(leftInfo.height,rightInfo.height)+1;
+        int height = Math.max(leftInfo.height, rightInfo.height) + 1;
         int p1 = leftInfo.maxDistance;
         int p2 = rightInfo.maxDistance;
         int p3 = leftInfo.height + rightInfo.height + 1;
