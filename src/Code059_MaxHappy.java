68cd4d2620673f6272ce35186797b4bf530f2fd9
alex <alex@gmail.com>
2022-10-29 16:19:24 +0800

    注释

--- src/Code059_MaxHappy.java
+++ src/Code059_MaxHappy.java
@@ -6,12 +6,13 @@ public class Code059_MaxHappy {
     // 公司的每个员工都符合 Employee 类的描述。
     // 整个公司的人员结构可以看作是一棵标准的、 没有环的多叉树。
     // 树的头节点是公司唯一的老板。除老板之外的每个员工都有唯一的直接上级。
-    // 叶节点是没有任何下属的基层员工(subordinates列表为空)，除基层员工外，每个员工都有一个或多个直接下级。
+    // 叶节点是没有任何下属的基层员工(nexts列表为空)，除基层员工外，每个员工都有一个或多个直接下级。
     // 这个公司现在要办party，你可以决定哪些员工来，哪些员工不来.
     // 规则：
     // 1.如果某个员工来了，那么这个员工的所有直接下级都不能来
     // 2.派对的整体快乐值是所有到场员工快乐值的累加
-    // 3.你的目标是让派对的整体快乐值尽量大 给定一棵多叉树的头节点boss，
+    // 3.你的目标是让派对的整体快乐值尽量大
+    // 给定一棵多叉树的头节点boss，
     // 请返回派对的最大快乐值。
     public static class Employee {
         public int happy; // 这名员工可以带来的快乐值
@@ -34,8 +35,8 @@ public class Code059_MaxHappy {
     // 当前来到的节点叫cur，
     // up表示cur的上级是否来，
     // 该函数含义：
-    // 如果up为true，表示在cur上级已经确定来，的情况下，cur整棵树能够提供最大的快乐值是多少？
-    // 如果up为false，表示在cur上级已经确定不来，的情况下，cur整棵树能够提供最大的快乐值是多少？
+    // 如果up为true，表示在cur上级已经确定来的情况下，cur整棵树能够提供最大的快乐值是多少？
+    // 如果up为false，表示在cur上级已经确定不来的情况下，cur整棵树能够提供最大的快乐值是多少？
     public static int process1(Employee cur, boolean up) {
         if (up) { // 如果cur的上级来的话，cur没得选，只能不来
             int ans = 0;
@@ -44,8 +45,8 @@ public class Code059_MaxHappy {
             }
             return ans;
         } else { // 如果cur的上级不来的话，cur可以选，可以来也可以不来
-            int p1 = cur.happy;
-            int p2 = 0;
+            int p1 = cur.happy; // cur 来
+            int p2 = 0; // cur 不来
             for (Employee next : cur.nexts) {
                 p1 += process1(next, true);
                 p2 += process1(next, false);
@@ -60,8 +61,8 @@ public class Code059_MaxHappy {
     }
 
     public static class Info {
-        public int no;
-        public int yes;
+        public int no; // 当前头节点不来的最大快乐值
+        public int yes; // 当前头节点来的最大快乐值
 
         public Info(int no, int yes) {
             this.no = no;
@@ -110,10 +111,10 @@ public class Code059_MaxHappy {
 
     // for test
     public static void main(String[] args) {
-        int maxLevel = 10;
-        int maxNexts = 10;
+        int maxLevel = 5;
+        int maxNexts = 5;
         int maxHappy = 100;
-        int testTimes = 10000;
+        int testTimes = 1000;
         for (int i = 0; i < testTimes; i++) {
             Employee boss = generateBoss(maxLevel, maxNexts, maxHappy);
             if (maxHappy1(boss) != maxHappy2(boss)){
