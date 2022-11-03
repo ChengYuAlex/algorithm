8bf8fcdae61b6536358608fe2f518b0673ea10d3
alex <alex@gmail.com>
2022-11-04 07:50:57 +0800

    code66 省份的数量(朋友圈)

--- src/Code066_FriendCircles.java
+++ src/Code066_FriendCircles.java
@@ -2,6 +2,7 @@ import java.sql.Array;
 
 public class Code066_FriendCircles {
     // 省份的数量(朋友圈)
+    // 测试链接：https://leetcode.com/problems/number-of-provinces/
     // 有 n 个城市，其中一些彼此相连，另一些没有相连。
     // 如果城市 a 与城市 b 直接相连，
     // 且城市 b 与城市 c 直接相连，
@@ -11,7 +12,6 @@ public class Code066_FriendCircles {
     // 其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，
     // 而 isConnected[i][j] = 0 表示二者不直接相连。
     // 返回矩阵中 省份 的数量。
-    // 测试链接：https://leetcode.com/problems/number-of-provinces/
 
     public static int findCircleNum(int[][] M) {
         int N = M.length;
