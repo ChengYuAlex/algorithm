6180108e3ddff4f7cdf5ee13647394cda8be8f03
alex <alex@gmail.com>
2022-11-09 22:43:53 +0800

    code67 岛的数量

--- src/Code067_NumberOfIslands.java
+++ src/Code067_NumberOfIslands.java
@@ -5,6 +5,10 @@ import java.util.Stack;
 
 public class Code067_NumberOfIslands {
     // 测试链接：https://leetcode.com/problems/number-of-islands/
+    // 岛的数量
+    // 给你一个由'1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+    // 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+    // 此外，你可以假设该网格的四条边均被水包围。
 
     public static int numIslands3(char[][] board) {
         int islands = 0;
