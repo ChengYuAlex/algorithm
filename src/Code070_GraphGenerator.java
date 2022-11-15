595a794b69465b5567ebcaa2301f662f621d9c6e
alex <alex@gmail.com>
2022-11-15 17:15:25 +0800

    code070 图生成器

---
+++ src/Code070_GraphGenerator.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Code070_GraphGenerator {
+    // 图生成器
+    // 用于处理输入
+
+    // matrix 所有的边
+    // N*3 的矩阵
+    // [weight, from节点上面的值，to节点上面的值]
+    //
+    // [ 5 , 0 , 7]
+    // [ 3 , 0,  1]
+    //
+    public static Graph createGraph(int[][] matrix) {
+        Graph graph = new Graph();
+        for (int i = 0; i < matrix.length; i++) {
+            // 拿到每一条边， matrix[i]
+            int weight = matrix[i][0];
+            int from = matrix[i][1];
+            int to = matrix[i][2];
+            if (!graph.nodes.containsKey(from)) {
+                graph.nodes.put(from, new Node(from));
+            }
+            if (!graph.nodes.containsKey(to)) {
+                graph.nodes.put(to, new Node(to));
+            }
+            Node fromNode = graph.nodes.get(from);
+            Node toNode = graph.nodes.get(to);
+            Edge newEdge = new Edge(weight, fromNode, toNode);
+            fromNode.nexts.add(toNode);
+            fromNode.out++;
+            toNode.in++;
+            fromNode.edges.add(newEdge);
+            graph.edges.add(newEdge);
+        }
+        return graph;
+    }
+}
+
+class Node {
+    // 节点值
+    public int value;
+    // 入度 指向该节点的边数
+    public int in;
+    // 出度 该节点指向的边数
+    public int out;
+    // 邻居 该节点指向的节点
+    public ArrayList<Node> nexts;
+    // 边 该节点指向邻居的边
+    public ArrayList<Edge> edges;
+
+    public Node(int value) {
+        this.value = value;
+        in = 0;
+        out = 0;
+        nexts = new ArrayList<>();
+        edges = new ArrayList<>();
+    }
+}
+
+class Edge {
+    public int weight;
+    public Node from;
+    public Node to;
+
+    public Edge(int weight, Node from, Node to) {
+        this.weight = weight;
+        this.from = from;
+        this.to = to;
+    }
+}
+
+class Graph {
+    public HashMap<Integer, Node> nodes;
+    public HashSet<Edge> edges;
+
+    public Graph() {
+        nodes = new HashMap<>();
+        edges = new HashSet<>();
+    }
+}
